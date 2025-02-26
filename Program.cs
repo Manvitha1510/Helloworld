7c332f8dcf1fc12ae48cb7bfbde138fa77c74e2f
Manvitha <manvi15.challagundla@gmail.com>
2025-02-26 17:33:25 +0200

    2nd line

--- Program.cs
+++ Program.cs
@@ -8,7 +8,9 @@ namespace Helloworld
         {
             Debug.WriteLine("Hello, World!");
             Debug.WriteLine("New message");
+            Debug.WriteLine("3rdLine");
             Console.ReadLine();
+
 
             int x;
             x = 3;
